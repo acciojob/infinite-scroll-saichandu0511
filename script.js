281d286b4244338d77b93ae5fc5dbb6f5cd78f80
saichandu0511 <saichandanreddy226@gmail.com>
2026-09-20 13:28:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your code here!
 let Infinite Scroll = document.getElementById("infi-list");
-for (let i=1;i<=10;i++);{
+for (let i=1;i<=10;i++){
 let li=document.createElement("li");
 li.innerText"item"+i;
-	infi-list.appendChild(i);
+infi-list.appendChild(li))
 }
 window.addEventListener("scroll", function () {
     if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
