a5c4ccba40bcd61a272d02dba3bc395c1635b326
saichandu0511 <saichandanreddy226@gmail.com>
2026-09-20 13:26:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here!
+let Infinite Scroll = document.getElementById("infi-list");
+for (let i=1;i<=10;i++);{
+let li=document.createElement("li");
+li.innerText"item"+i;
+	infi-list.appendChild(i);
+}
+window.addEventListener("scroll", function () {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        
+        let currentItems = list.children.length;
+
+        for (let i = 1; i <= 2; i++) {
+            let li = document.createElement("li");
+            li.innerText = "Item " + (currentItems + i);
+            list.appendChild(li);
+        }
+    }
+});
 
